b941466ccef30941ab5a8c69b2c957396fa0adaf
Taras Skumin <taras.skumin@gmail.com>
2021-07-06 18:31:27 +0300

    fonts, text styles, color resources

--- lib/resources/color_resources.dart
+++ lib/resources/color_resources.dart
@@ -1,3 +1,9 @@
-class WeStriveColors { 
-  
+import 'package:flutter/material.dart';
+
+class StepCounterColors {
+  static const Color orange = Color.fromRGBO(247, 165, 108, 1);
+  static const Color fadeGray = Color.fromRGBO(237, 241, 243, 1);
+  static const Color gray = Color.fromRGBO(166, 172, 180, 1);
+  static const Color softBlue = Color.fromRGBO(89, 98, 116, 1);
+  static const Color darkBlue = Color.fromRGBO(31, 52, 85, 1);
 }
--- lib/resources/text_style_resources.dart
+++ lib/resources/text_style_resources.dart
@@ -1,3 +1,4 @@
+import 'package:fastic_flutter_test/resources/color_resources.dart';
 import 'package:flutter/material.dart';
 
 class StepCounterFontFamily {
@@ -5,9 +6,39 @@ class StepCounterFontFamily {
 }
 
 class StepCounterTextStyle {
-  static const TextStyle latoBlack = TextStyle(
+  static const TextStyle title = TextStyle(
     fontFamily: StepCounterFontFamily.lato,
+    fontSize: 28,
     fontWeight: FontWeight.w900,
     fontStyle: FontStyle.normal,
+    color: StepCounterColors.darkBlue,
+  );
+  static const TextStyle chart = TextStyle(
+    fontFamily: StepCounterFontFamily.lato,
+    fontSize: 32,
+    fontWeight: FontWeight.w900,
+    fontStyle: FontStyle.normal,
+    color: StepCounterColors.darkBlue,
+  );
+  static const TextStyle amount = TextStyle(
+    fontFamily: StepCounterFontFamily.lato,
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    fontStyle: FontStyle.normal,
+    color: StepCounterColors.darkBlue,
+  );
+  static const TextStyle description = TextStyle(
+    fontFamily: StepCounterFontFamily.lato,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    color: StepCounterColors.softBlue,
+  );
+  static const TextStyle dailyGoal = TextStyle(
+    fontFamily: StepCounterFontFamily.lato,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontStyle: FontStyle.normal,
+    color: StepCounterColors.gray,
   );
 }
